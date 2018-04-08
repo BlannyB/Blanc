c77f8f385d16cf1477e0aaa305f5e30b6115e3e4
BlannyB <38150180+BlannyB@users.noreply.github.com>
2018-04-08 03:29:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
+client.login(process.env.NDMyMzcwMzI5ODk4NDUwOTQ0.DatWSA.fNohV9A5sOqCmm1o9QS703GpMXo);//where BOT_TOKEN is the token of our bot
